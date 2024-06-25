664b872a2d6da9e7beb8ebbbb8785f73ac2b2080
ranerlich7 <ranerlich@gmail.com>
2024-06-25 20:23:42 +0300

    added templates

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-my_contacts = [{"name": "Ran", "age": 44}, {"name": "Aviad", "age": 25}]
+my_contacts = [
+    {"name": "Ran", "age": 44, "phone":"05544444"},
+    {"name": "Aviad", "age": 25, "phone":"050444222"}
+]
 
 
 @app.route("/")
 def contact_list():
-    return f"{my_contacts}"
+    return render_template('contact_list.html', contacts = my_contacts)
+
+    # final_str = ""
+    # for contact in my_contacts:
+    #     final_str += f"<li style='color:blue'>{contact["name"]} - {contact["phone"]}</li>"
+    # return final_str
 
 
-@app.route("/single_contact/<int:usernumber>")
-def single_contact(usernumber):
-    return f"user is {usernumber}"
+@app.route("/single_contact/<int:index>")
+def single_contact(index):
+    return f"user is {my_contacts[index]["name"]} - {my_contacts[index]["phone"]}"
 
 
 @app.route("/add")
